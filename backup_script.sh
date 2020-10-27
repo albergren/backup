26c54be167a428370eeec9f4fb1f6abf913af4cc
Martin Jensen <albergren@users.noreply.github.com>
2020-10-27 22:02:48 +0100

    Find and mount drives and create archive on drive

---
+++ backup_script.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+. backup.conf
+
+backup_created=false
+backup_destination=""
+
+function create_archive () {
+    time=`date +%b-%d-%y`
+    filename=backup-$time.tar.gz 
+    src_dirs=''
+
+    for i in "${dir_paths[@]}"; do
+    
+	dir_path=$(dirname $i)
+	dir_name=$(basename $i)
+   
+	src_dirs="$src_dirs -C  $dir_path $dir_name" 
+    done
+
+    tar -cpvzf $1$filename $src_dirs
+    backup_created=true 
+
+}
+
+
+function mount_drives () {
+    for i in /dev/sd[b-e][1-9]; do
+	udisksctl mount -b $i  
+    done
+    sleep 5
+}
+
+function find_backup_location() {
+    mount_point=/media/`whoami`
+    for i in $mount_point/* ; do
+	if [ -f $i/.backup_dest ] ; then
+	    echo "success"
+	    backup_destination=$i/
+	    return 1;
+	fi
+    done
+    echo "no show"
+    return 0;
+
+}
+function remove_oldest_backup () {
+    echo ""
+	}
+
+# If first run then initialize
+if  [ ! -f .backup_dest ] ; then
+    read -p "Enter path to location of backup on drive: " destination
+    touch $destination/.backup_dest    
+    touch .backup_dest
+    
+else   
+    # find mountpont for drive and create archive. 
+    find_backup_location
+    if [ "$backup_destination"  ] ; then
+	create_archive $backup_destination
+	exit 1
+    fi
+    # If not found mount drives and try again.
+    mount_drives
+    find_backup_location
+    if [ "$backup_destination"  ] ; then
+	create_archive $backup_destination
+	exit 1
+    fi
+
+fi
