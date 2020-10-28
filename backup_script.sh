395e89662f63ce8bd34afd6fd215256277273865
Martin Jensen <albergren@users.noreply.github.com>
2020-10-28 12:17:59 +0100

    Remove oldest backup

--- backup_script.sh
+++ backup_script.sh
@@ -4,10 +4,10 @@
 
 backup_created=false
 backup_destination=""
-identifier_name=".backup_dir"
+identifier_name=".backup_identifier"
 
 function create_archive () {
-    time=`date +%b-%d-%y`
+    time=`date +%G%m%d%H%M`
     filename=backup-$time.tar.gz 
     src_dirs=''
 
@@ -27,6 +27,7 @@ function create_archive () {
 
 function mount_drives () {
     for i in /dev/sd[b-e][1-9]; do
+	echo "Mounting connected drives..."
 	udisksctl mount -b $i  
     done
     sleep 5
@@ -35,40 +36,50 @@ function mount_drives () {
 function find_backup_location() {
     mount_point=/media/`whoami`
     for i in $mount_point/* ; do
-	echo $identifier_name
 	if [ -f $i/$identifier_name ] ; then
-	    echo "success"
+	    echo "Backup drive found!"
 	    backup_destination=$i/
 	    return 1;
 	fi
     done
-    echo "no show"
+    echo "Backup drive could not be found. Make sure drive is connected!"
     return 0;
 
 }
 function remove_oldest_backup () {
-    echo ""
-	}
+    backup_count=$(find $1backup-*.tar.gz | sort | wc -l)
+    if (( $backup_count > $store_count ))  ; then
+	oldest_backup=`find $1backup-*.tar.gz | sort | head -n1`
+	rm $oldest_backup
+    fi
+}
 
 # If first run then initialize
 if  [ ! -f $identifier_name ] ; then
-    read -p "Enter path to location of backup on drive: " destination
+    read -p "Enter path to drive where backups will be stored: " destination
     touch $destination/$identifier_name    
     touch $identifier_name
     
 else   
     # find mountpont for drive and create archive. 
     find_backup_location
+    
     if [ "$backup_destination"  ] ; then
+	echo "Creating archive..."
 	create_archive $backup_destination
+	echo "Archive created!"
+	remove_oldest_backup  $backup_destination
 	exit 1
     fi
     # If not found mount drives and try again.
     mount_drives
     find_backup_location
+    
     if [ "$backup_destination"  ] ; then
+	echo "Creating archive..."
 	create_archive $backup_destination
+	echo "Archive created!"
+	remove_oldest_backup $backup_destination
 	exit 1
     fi
-
 fi
